cd456d0b89c5e793dc4f3389cf0de8f50c3cbd7a
hiroaki.kobayashi <hiroaki.kobayashi@compass-e.com>
2025-07-22 18:56:39 +0900

    statement関数の分割_変数名の変更：thisAmount->result

--- main.js
+++ main.js
@@ -10,21 +10,21 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
+        let result = amountFor(perf, play);
 
         switch (play.type) {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
+                    result += 1000 * (perf.audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                    result += 10000 + 500 * (perf.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                result += 300 * perf.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
@@ -35,8 +35,8 @@ function statement(invoice, plays) {
         // 喜劇のときは10人につき、さらにポイントを加算
         if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
         // 注文の内訳を出力
-        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        result += `  ${play.name}: ${format(result / 100)} (${perf.audience} seats)\n`;
+        totalAmount += result;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
@@ -44,25 +44,25 @@ function statement(invoice, plays) {
 }
 
 function amountFor(perf, play) {
-    let thisAmount = 0;
+    let result = 0;
     switch (play.type) {
         case "tragedy":
-            thisAmount = 40000;
+            result = 40000;
             if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+                result += 1000 * (perf.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
+            result = 30000;
             if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
+                result += 10000 + 500 * (perf.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * perf.audience;
             break;
         default:
             throw new Error(`unknown type: ${play.type}`);
     }
-    return thisAmount;
+    return result;
 }
 
 
